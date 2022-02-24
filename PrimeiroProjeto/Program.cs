3f8ea3b700391596343a176784a24281c0fa7a66
João Victor Costa Alves <57672775+jxvictor@users.noreply.github.com>
2022-02-24 01:38:30 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PrimeiroProjeto;
+
+//Console.WriteLine("Hello, World!");
+
+Pessoa novaPessoa = new Pessoa("João Victor", 21);
+
+Console.WriteLine("Nome: " + novaPessoa.ObterNome());
+Console.WriteLine("Idade: " + novaPessoa.ObterIdade());
