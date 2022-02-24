fb2d9d171dca14d2cf8e392ec3f744718209a109
João Victor Costa Alves <57672775+jxvictor@users.noreply.github.com>
2022-02-24 01:38:30 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/Pessoa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto
+{
+    public class Pessoa
+    {
+        private string nome;
+        private int idade;
+
+        public Pessoa(string nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+        }
+
+        public string ObterNome() 
+        { 
+            return this.nome;
+        }
+
+        public int ObterIdade() 
+        { 
+            return this.idade;
+        }
+    }
+}
